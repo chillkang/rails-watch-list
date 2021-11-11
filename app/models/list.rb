ea8ce99ff574568952337db3aab15742cd1f67e6
Seulkee Kang <seulkeekang@gmail.com>
2021-11-11 15:15:18 +0000

    created a static seed for movies

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,4 @@
-class List < ApplicationRecor
+class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
